dcf8eb610e3be95492ef2822b565ac2ebd5eb986
kubryk <flock1715@gmail.com>
2026-03-03 10:31:36 +0200

    Pass REDIS_PASSWORD to BullModule connection and docker-compose.yml

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,6 +15,7 @@ import { ProcessorModule } from './processor/processor.module';
       connection: {
         host: process.env.REDIS_HOST || 'localhost',
         port: parseInt(process.env.REDIS_PORT || '6379', 10),
+        password: process.env.REDIS_PASSWORD || undefined,
       },
     }),
     PrismaModule,
